2491578034af83fa2dc0e22e24a6a39158d47484
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2020-12-14 16:29:59 +0100

    vue-hello messaggio stampato

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
  /* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus: Aggiungere un’immagine presa anch’essa da un data */
+
+
+let app = new Vue({
+    //mettiamo una serie di proprietà
+    el: "#root",
+    data: {
+            message: "Che bella la vita con un bicchiere di vino!",
+        }
+}) 
